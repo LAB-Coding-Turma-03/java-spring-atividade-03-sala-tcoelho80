ce6bd0ac142f8eb73c7b89ef71f79aa22dd75026
tcoelho80 <51813749+tcoelho80@users.noreply.github.com>
2021-05-21 18:51:04 -0300

    Update and rename src/main/java/br/com/impacta/lab/controller/CondicionalController.java to src/a Principal/Java/br/com/impacta/laboratório/controlador/CondicionalController.java
    Alterado arquivo de controle

--- src/a Principal/Java/br/com/impacta/laboratório/controlador/CondicionalController.java
+++ src/a Principal/Java/br/com/impacta/laboratório/controlador/CondicionalController.java
@@ -12,21 +12,19 @@ public class CondicionalController {
 
 	@GetMapping("/condicional")
 	public ResponseEntity<String> simularValores(@RequestParam(name="idade") int idade) {
-		/*
-		 * Elabore um algoritmo para dizer a partir da idade se a 
-		 * pessoa tem mais ou menos do que 18 anos.
-		 *  
-		 *  Retornar os seguintes textos:
-		 *  
-		 *  
-		 *  Para maiores de dezoito anos -> Possui mais de 18 anos
-		 *  
-		 *  Para menores de dezoito anos -> Possui menos de 18 anos
-		 *  
-		 */
+		int idadeCorte = 18;
+		
+		//int idadeEntrada = Integer.parseInt(idade);
+		var retorno = new String();
+
+		if (idade > idadeCorte){
+			retorno = "Possui mais de 18 anos";
+		}else if (idade < idadeCorte){
+			retorno = "Possui menos de 18 anos";
+		}
 		
 		
-		return ResponseEntity.ok("APÓS O ALGORITMO COLOCAR O TEXTO/VARIAVEL DE RETORNO AQUI");
+		return ResponseEntity.ok(retorno);
 	}
 	
 }
